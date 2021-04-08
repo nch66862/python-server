78f27c7c7e054e90a9ead0da773fa819471b270d
Nick Carver <nicholascarver2@outlook.com>
2021-04-08 15:44:14 -0500

    implemented chapter 4 post requests for all tables

--- animals/__init__.py
+++ animals/__init__.py
@@ -1,2 +1,4 @@
 #.request means in the same folder and in the request file
-from .request import get_all_animals, get_single_animal
+from .request import get_all_animals
+from .request import get_single_animal
+from .request import create_animal
--- customers/__init__.py
+++ customers/__init__.py
@@ -1,1 +1,3 @@
-from .request import get_all_customers, get_single_customer
+from .request import get_all_customers
+from .request import get_single_customer
+from .request import create_customer

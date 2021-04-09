892f528560807e92fa1717714fd726c24c470a69
Nick Carver <nicholascarver2@outlook.com>
2021-04-09 12:00:27 -0500

    set up all models

---
+++ models/employee.py
@@ -0,0 +1,7 @@
+class Employee():
+
+    def __init__(self, id, name, role, location_id):
+        self.id = id
+        self.name = name
+        self.role = role
+        self.location_id = location_id

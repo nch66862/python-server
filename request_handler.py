435bf7edb7fc15a08999c52c8777712e4f138676
Nick Carver <nicholascarver2@outlook.com>
2021-04-09 11:20:53 -0500

    able to do put requests on all resources

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,6 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal, create_animal, delete_animal
+from animals import get_all_animals, get_single_animal, create_animal, delete_animal, update_animal
 from customers import get_all_customers, get_single_customer, create_customer, delete_customer
 from employees import get_all_employees, get_single_employee, create_employee, delete_employee
 from locations import get_all_locations, get_single_location, create_location, delete_location
@@ -124,7 +124,26 @@ class HandleRequests(BaseHTTPRequestHandler):
     # It handles any PUT request.
 
     def do_PUT(self):
-        self.do_POST()
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            update_animal(id, post_body)
+        elif resource == "customers":
+            update_customer(id, post_body)
+        elif resource == "employees":
+            update_employee(id, post_body)
+        elif resource == "locations":
+            update_location(id, post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
 
     def do_DELETE(self):
         # Set a 204 response code

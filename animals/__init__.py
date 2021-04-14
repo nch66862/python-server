5a7132e52e42cc8612312d9c6b6c33d335da4e5e
Nick Carver <nicholascarver2@outlook.com>
2021-04-14 13:38:11 -0500

    completed chapter 9

--- animals/__init__.py
+++ animals/__init__.py
@@ -5,3 +5,4 @@ from .request import create_animal
 from .request import delete_animal
 from .request import update_animal
 from .request import get_animals_by_location
+from .request import get_animals_by_status
--- employees/request.py
+++ employees/request.py
@@ -107,3 +107,41 @@ def update_employee(id, new_employee):
             # Found the employee. Update the value.
             EMPLOYEES[index] = new_employee
             break
+
+def get_employees_by_location(locationId):
+    with sqlite3.connect("./kennel.db") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Use a ? parameter to inject a variable's value
+        # into the SQL statement.
+        db_cursor.execute("""
+        SELECT
+            e.id,
+            e.name,
+            e.address,
+            e.location_id
+        FROM employee e
+        WHERE e.location_id = ?
+        """, ( locationId, ))
+
+        # Initialize an empty list to hold all employee representations
+        employees = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an employee instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # Employee class above.
+            employee = Employee(row['id'], row['name'], row['address'],
+                            row['location_id'])
+
+            employees.append(employee.__dict__)
+
+    # Use `json` package to properly serialize list as JSON
+    return json.dumps(employees)
